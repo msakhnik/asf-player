0c0e47dcdde6349d6b5a89341cfafaceb7a9a9b7
Misha Sakhnik <kskster@gmail.com>
2011-12-27 22:02:16 +0200

    +Добавив довгі опції

--- main.cpp
+++ main.cpp
@@ -9,48 +9,61 @@
 #include "AsfPlayer.h"
 
 #include <iostream>
+#include <getopt.h>
 
 using namespace std;
 
 int main(int argc, char** argv)
 {
-    int oc;
     string filename = "";
 
     bool full_screen = false;
     bool frame_by_frame = false;
 
-    while ((oc = getopt(argc, argv, "p:hfbs")) != -1)
+    static struct option long_options[] = {
+        {"frame_by_frame", no_argument, 0, 'b'},
+        {"path", required_argument, 0, 'p'},
+        {"full_screen", no_argument, 0, 'f'},
+        {"help", no_argument, 0, 'h'},
+    };
+
+    int c = 0;
+    int option_index = 0;
+
+    c = getopt_long(argc, argv, "bfhp:",
+                    long_options, &option_index);
+
+    if (c == -1)
+        return 1;
+
+    switch (c)
     {
-        switch (oc)
-        {
-        case 'h':
-            cout << "Help!\n"
-                    "-f - run on full screen\n"
-                    "-p - path to video file\n"
-                    "-b - frame by frame\n"
-                    "-h - help\n\n"
-                    "Example:\n"
-                    "./player -p example/example2.asf -f\n"
-                    "or ./player (load default video example1.asf)\n"
-                    "this created test.asf file into example directory"
-                 << endl;
+    case 'h':
+        cout << "Help!\n"
+            "-f - run on full screen\n"
+            "-p - path to video file\n"
+            "-b - frame by frame\n"
+            "-h - help\n\n"
+            "Example:\n"
+            "./player -p example/example2.asf -f\n"
+            "or ./player (load default video example1.asf)\n"
+            "this created test.asf file into example directory"
+            << endl;
 
-            return 0;
+        return 0;
 
-        case 'f':
-            full_screen = true;
-            break;
-        case 'b':
-            frame_by_frame = true;
-            break;
-        case 'p':
-            filename = optarg;
-            break;
-        default:
-            cerr << "input error" << endl;
-            break;
-        }
+    case 'f':
+        full_screen = true;
+        break;
+    case 'b':
+        frame_by_frame = true;
+        break;
+    case 'p':
+        filename = optarg;
+        break;
+    default:
+        cerr << "input error" << endl;
+        break;
     }
 
     if (filename.empty())
