c4364dcdf4ea108399b726531448194a72781352
Anatoliy Sakhnik <sakhnik@gmail.com>
2011-12-29 01:28:55 +0200

    Fixed synopsis to more natural

--- src/main.cpp
+++ src/main.cpp
@@ -19,47 +19,36 @@ static char const* _Basename(char const* fname)
     return res ? res + 1 : fname;
 }
 
-static void _ReadHelp(const char * progname)
+static void _ReadHelp(const char *progname)
 {
     cout << "ASF video player\n\n"
-                "Synopsis:\n"
-                "  " << progname << " [options]\n"
-                "  " << progname <<" [options] [file]\n\n"
-                "Options:\n"
-                "  -f,--full-screen\tPlay on full screen\n"
-                "  -p,--path\t\tPath to video file\n"
-                "  -b,--frame-by-frame\tShow frame by frame\n"
-                "  -h,--help\t\tThis help message\n\n"
-                "Example:\n"
-                "  " << progname << " example/example1.asf\n"
-                "  " << progname << " -p example/example2.asf -f\n"
-                "  or " << progname << " (load default video example1.asf)\n"
-                "  this creates a file test.asf in the directory 'example'"
-                << endl;
+            "Synopsis:\n"
+            "  " << progname <<" [options] file\n\n"
+            "Options:\n"
+            "  -f,--full-screen\tPlay on full screen\n"
+            "  -b,--frame-by-frame\tShow frame by frame\n"
+            "  -h,--help\t\tThis help message\n\n"
+            "Example:\n"
+            "  " << progname << " example/example1.asf\n"
+            "  " << progname << " -f example/example2.asf\n"
+            "  this creates a file test.asf in the directory 'example'"
+         << endl;
 }
 
 int main(int argc, char** argv)
 {
     char const* progname = _Basename(argv[0]);
-    string filename;
 
     bool full_screen = false;
     bool frame_by_frame = false;
 
-     if(argc == 1)
-     {
-         _ReadHelp(progname);
-         return 0;
-     }
-
     int counter = 0;
-    while (1)
+    while (true)
     {
         ++counter;
         static struct option long_options[] =
         {
             { "frame-by-frame", no_argument,        0, 'b' },
-            { "path",           required_argument,  0, 'p' },
             { "full-screen",    no_argument,        0, 'f' },
             { "help",           no_argument,        0, 'h' },
             { 0, 0, 0, 0 }
@@ -88,47 +77,42 @@ int main(int argc, char** argv)
             frame_by_frame = true;
             break;
 
-        case 'p':
-            filename = optarg;
-            break;
-
         default:
-            cerr << "input error" << endl;
-            break;
+            cerr << "Unknown option '" << c << "'" << endl;
+            return 1;
         }
     }
 
-    if (argc != counter && filename.empty())
+    if (argc != counter + 1)
     {
-        filename = argv[argc - 1];
-        cout << filename << endl;
-    }
-
-    if (filename.length() > 4 &&
-        filename.substr(filename.length() - 4, 4) == ".asf")
-    {
-
-        cAsfFile file(filename);
-
-        cAsfPlayer player(file);
-
-        if (!player.Init())
-            return 1;
-
-        if (full_screen)
-            player.SetFullScreen(true);
-
-        if (frame_by_frame)
-            player.SetFrameByFrame(true);
-
-        player.Play();
-    }
-    else
-    {
-        cerr << "Error syntax is incorrect!\nSyntax: " << argv[ 0 ]
-            << " <file>" << endl;
+        cerr << "File name is missing (try --help)" << endl;
         return 1;
     }
+
+    string filename = argv[counter];
+    cout << filename << endl;
+
+    if (filename.length() < 4 ||
+        filename.substr(filename.length() - 4, 4) != ".asf")
+    {
+        cerr << "File '" << filename << "' isn't supported" << endl;
+        return 1;
+    }
+
+    cAsfFile file(filename);
+
+    cAsfPlayer player(file);
+
+    if (!player.Init())
+        return 1;
+
+    if (full_screen)
+        player.SetFullScreen(true);
+
+    if (frame_by_frame)
+        player.SetFrameByFrame(true);
+
+    player.Play();
 
     return 0;
 }
