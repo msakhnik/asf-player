e44378e7031876f2344549c772050fddadf10488
Misha Sakhnik <kskster@gmail.com>
2012-01-02 21:02:34 +0200

    +Put License notice to the file header

--- src/AsfPlayer.cpp
+++ src/AsfPlayer.cpp
@@ -1,3 +1,24 @@
+//
+// AsfPlayer.cpp
+//
+//     Created: 02.12.2012
+//      Author: Sakhnik
+//
+// This file is part of Asf Player.
+//
+// Asf Player is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Asf Player License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Asf Player is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Asf Player.  If not, see <http://www.gnu.org/licenses/>
+
 #include "AsfPlayer.h"
 #include "AsfFile.h"
 
@@ -113,7 +134,7 @@ void cAsfPlayer::_SetPlayerOptions(unsigned int &frame, unsigned int & end)
 {
     //put trackbar in top
     if (_track)
-        cvCreateTrackbar2("Position", "frame", reinterpret_cast<int*>
+        cvCreateTrackbar2("Frame:", "frame", reinterpret_cast<int*>
                           (&frame), static_cast<int> (end), TrackbarCallback, this);
     if (_scale)
     {
